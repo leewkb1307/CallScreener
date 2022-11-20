7c9fbe391691d5a9fa29105fb09dfea0e569e459
Bernard Lee <leewkb1307@gmail.com>
2022-11-20 14:09:37 +0800

    Request call screener role.

--- app/src/main/java/com/gmail/leewkb1307/callscreener/SettingsActivity.kt
+++ app/src/main/java/com/gmail/leewkb1307/callscreener/SettingsActivity.kt
@@ -1,6 +1,11 @@
 package com.gmail.leewkb1307.callscreener
 
+import android.app.Activity
+import android.app.role.RoleManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 
@@ -16,11 +21,26 @@ class SettingsActivity : AppCompatActivity() {
                 .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
+        requestScreenerRole()
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+
+    private fun requestScreenerRole() {
+        val roleManager = getSystemService(ROLE_SERVICE) as RoleManager
+        val intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_CALL_SCREENING)
+        requestRoleLauncher.launch(intent)
+    }
+
+    private val requestRoleLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+        if (result.resultCode != Activity.RESULT_OK) {
+            Toast.makeText(this,"Call screener setup not good!", Toast.LENGTH_SHORT).show()
         }
     }
 }
