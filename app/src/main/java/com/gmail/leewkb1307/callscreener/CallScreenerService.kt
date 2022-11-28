9ea4816aa85dc25172dc5b26b9f02698f68453fb
Bernard Lee <leewkb1307@gmail.com>
2022-11-28 17:52:38 +0800

    Option to allow all calls.

--- app/src/main/java/com/gmail/leewkb1307/callscreener/CallScreenerService.kt
+++ app/src/main/java/com/gmail/leewkb1307/callscreener/CallScreenerService.kt
@@ -1,11 +1,37 @@
 package com.gmail.leewkb1307.callscreener
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.telecom.Call
+import android.telecom.Call.Details.DIRECTION_INCOMING
 import android.telecom.CallScreeningService
+import androidx.preference.PreferenceManager
+
 
 class CallScreenerService : CallScreeningService() {
     override fun onScreenCall(callDetails: Call.Details) {
-        respondToCall(callDetails,
-            CallResponse.Builder().setDisallowCall(true).setRejectCall(true).build())
+        if (callDetails.callDirection == DIRECTION_INCOMING) {
+            var isEndCall = false
+            val context: Context = applicationContext
+            val sharedPref: SharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(context)
+            val prefMode = sharedPref.getString("prefMode", "allow_all")
+
+            if (prefMode == "block_all") {
+                isEndCall = true
+            }
+
+            val callResponseBuilder: CallResponse.Builder = CallResponse.Builder()
+
+            if (isEndCall) {
+                callResponseBuilder.apply {
+                    setDisallowCall(true)
+                    setRejectCall(true)
+                }
+            }
+
+            respondToCall(callDetails,
+                callResponseBuilder.build())
+        }
     }
 }
