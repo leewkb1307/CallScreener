ce2f58ff594d159169033e469d435b4a80c3bcb3
Bernard Lee <leewkb1307@gmail.com>
2022-11-28 18:05:44 +0800

    Option to block unknown calls.

--- app/src/main/java/com/gmail/leewkb1307/callscreener/CallScreenerService.kt
+++ app/src/main/java/com/gmail/leewkb1307/callscreener/CallScreenerService.kt
@@ -2,15 +2,33 @@ package com.gmail.leewkb1307.callscreener
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.net.Uri
 import android.telecom.Call
 import android.telecom.Call.Details.DIRECTION_INCOMING
 import android.telecom.CallScreeningService
+import android.util.Log
 import androidx.preference.PreferenceManager
 
 
 class CallScreenerService : CallScreeningService() {
+    private val mLogTAG = "CallScreener"
+
     override fun onScreenCall(callDetails: Call.Details) {
         if (callDetails.callDirection == DIRECTION_INCOMING) {
+            val handle: Uri? = callDetails.handle
+
+            val incomingNumber: String? =
+                handle?.toString()?.replace("tel:", "")?.replace("%2B", "+")
+
+            if (incomingNumber == null) {
+                Log.d(mLogTAG, "Incoming number is NULL!")
+            }
+            else {
+                Log.d(mLogTAG, "Incoming number --> $incomingNumber")
+            }
+
+            val isUnknownCall = incomingNumber == null || incomingNumber.isEmpty()
+            
             var isEndCall = false
             val context: Context = applicationContext
             val sharedPref: SharedPreferences =
@@ -19,6 +37,8 @@ class CallScreenerService : CallScreeningService() {
 
             if (prefMode == "block_all") {
                 isEndCall = true
+            } else if (prefMode == "block_unknown") {
+                isEndCall = isUnknownCall
             }
 
             val callResponseBuilder: CallResponse.Builder = CallResponse.Builder()
