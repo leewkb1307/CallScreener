2c8442544d57002adaa77b696c86181dd1f6333c
Bernard Lee <leewkb1307@gmail.com>
2025-08-20 23:51:11 +0800

    Fix ActionBar overlap in Android 15.

--- app/src/main/java/com/gmail/leewkb1307/callscreener/SettingsActivity.kt
+++ app/src/main/java/com/gmail/leewkb1307/callscreener/SettingsActivity.kt
@@ -7,10 +7,13 @@ import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 
@@ -18,9 +21,15 @@ class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.settings_activity)
         if (savedInstanceState == null) {
             loadScreen()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
 
